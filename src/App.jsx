6e10f0a415ba21e1577e9e7fbd261595ddad035e
Adebobola Oyedunmade <m.oyedunmade@gmail.com>
2025-04-11 22:23:19 +0100

    feat: added the projects section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import About from "./components/about";
 import Hero from "./components/hero";
 import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
 import Services from "./components/services";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Hero />
         <About />
         <Services />
+        <Projects />
       </main>
     </div>
   );
---
+++ src/components/Projects/index.jsx
@@ -0,0 +1,41 @@
+import styles from "./projects.module.scss";
+
+const Project = ({ title, details }) => {
+  return (
+    <div className={styles.project}>
+      <span></span>
+      <div>
+        <h2>{title}</h2>
+        <p>{details} </p>
+        <button>View Project</button>
+      </div>
+    </div>
+  );
+};
+
+const Projects = () => {
+  return (
+    <section className={styles.projects_section}>
+      <div className={styles.projects_header}>
+        <h2>Recent Projects</h2>
+        <button>View All</button>
+      </div>
+      <div className={styles.projects_container}>
+        <Project
+          title="Loren Ipsum"
+          details="Worem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+        />
+        <Project
+          title="Loren Ipsum"
+          details="Worem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+        />
+        <Project
+          title="Loren Ipsum"
+          details="Worem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
